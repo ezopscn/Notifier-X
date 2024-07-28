944c7a5069905e3c48310a8493c23eba3344f785
DK <ezopscn@gmail.com>
2024-07-28 22:23:19 +0800

    日志输出

--- cmd/start.go
+++ cmd/start.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"NotifierX/common"
 	"NotifierX/initialize"
+	"NotifierX/pkg/utils"
 	"context"
 	"fmt"
 	"github.com/spf13/cobra"
@@ -24,9 +25,23 @@ var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "参数化启动服务",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(common.Logo) // Logo
-		initialize.Config()      // 配置初始化
-		r := initialize.Router() // 路由初始化
+		fmt.Println(common.Logo)  // Logo
+		initialize.Config()       // 配置初始化
+		initialize.SystemLogger() // 系统日志初始化
+		initialize.AccessLogger() // 访问日志初始化
+		r := initialize.Router()  // 路由初始化
+
+		// 判断参数是否合法
+		if !utils.IsIPAddress(common.Config.System.Listen) {
+			log.Println("系统配置监听地址不合法")
+			return
+		}
+
+		// 检测端口是否合法
+		if !utils.IsPort(common.Config.System.Port) {
+			log.Println("系统配置监听端口不合法")
+			return
+		}
 
 		// 监听地址
 		listenAddress := fmt.Sprintf("%s:%s", common.Config.System.Listen, common.Config.System.Port)
